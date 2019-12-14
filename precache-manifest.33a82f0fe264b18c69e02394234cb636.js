596c6d5e748779657b6f6c4da6fa6eb4f52575a2
Ronak Sonigara <ronak.sonigara.2018@gmail.com>
2019-12-14 23:44:03 +0530

    Updates

---
+++ precache-manifest.33a82f0fe264b18c69e02394234cb636.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e286d78285e5ff54c84d184430ba49",
+    "url": "/github-cards/index.html"
+  },
+  {
+    "revision": "c77beb1c1c84f0f41ad4",
+    "url": "/github-cards/static/css/main.89961a2e.chunk.css"
+  },
+  {
+    "revision": "65e4c1268a8f2608c317",
+    "url": "/github-cards/static/js/2.1b791b4d.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/github-cards/static/js/2.1b791b4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c77beb1c1c84f0f41ad4",
+    "url": "/github-cards/static/js/main.7770b473.chunk.js"
+  },
+  {
+    "revision": "5c813b46beea2f8068ea",
+    "url": "/github-cards/static/js/runtime-main.18acbcfb.js"
+  }
+]);
